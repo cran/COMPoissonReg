ed48ffeafcdc54e659b7642e1489ab5d48ce61e0
Thomas Lotze <thomas.lotze@thomaslotze.com>
2010-01-10 00:00:00 +0000

    version 0.3.3

--- R/ComputeBetasAndNuHat.R
+++ R/ComputeBetasAndNuHat.R
@@ -7,7 +7,7 @@ ComputeBetasAndNuHat <- function(x,y,betainit,nuinit,max){
 # nuinit = initial nu value
 
 #create vector of ones
-   if(is.matrix(x)==TRUE) {onevec <- rep(1,length(x[,1]))} else onevec <- rep(1,length(x)) 
+   if(is.matrix(x)==TRUE || is.data.frame(x) == TRUE) {onevec <- rep(1,length(x[,1]))} else onevec <- rep(1,length(x)) 
 
 #create real X matrix, namely where 1st col is vector of 1s to incorporate beta0 effect
    newx <- cbind(onevec,x) 
